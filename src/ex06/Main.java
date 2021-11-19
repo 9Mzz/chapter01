d652a809cff0f83cb9b4353b992e55dc7597e214
구민석 <ser_om@naver.com>
2021-11-19 22:39:49 +0900

    11/19

--- src/ex06/Main.java
+++ src/ex06/Main.java
@@ -3,14 +3,19 @@ package ex06;
 public class Main {
 	public static void main(String[] args) {
 		
-		String[] argss = new String[3];
-		argss[0] = "구민석";
-		argss[1] = "구민석1";
-		argss[2] = "구민석2";
+//		String[] argss = new String[3];
+//		argss[0] = "구민석";
+//		argss[1] = "구민석1";
+//		argss[2] = "구민석2";
+//		
+//		
+//		for( int i=0; i<argss.length; i++) {
+//			System.out.println(argss[i]);
 		
-		
-		for( int i=0; i<argss.length; i++) {
-			System.out.println(argss[i]);
+		if (args[0].equals("-version")) {
+			System.out.println("ver0.78");
+		}else {
+			System.out.println("옵션값을 확인해 주세요.");
 		}
 		
 	}
